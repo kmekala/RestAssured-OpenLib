67658fedb4784a7f1ec8347ea27ec494707a5fe6
kalyan Mekala <mekala.kalyan@gmail.com>
2017-08-08 15:12:46 -0400

    add the file hello world

---
+++ src/test/java/com/openLib/RestAssured/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.openLib.RestAssured;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by mekak2 on 8/8/17.
+ */
+public class HelloWorld {
+
+
+    @Test
+    public void helloWor() {
+System.out.println("print this awesome) saying..." + "Hello World");
+
+    }
+}
